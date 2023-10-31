fb56cbe5f1f52a59bcd7c0dd09f56ba516928f5c
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2023-10-31 14:13:54 +0200

    Initial Commit

---
+++ swiftui-coordinator-pattern-ios16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-coordinator-pattern-ios16
+//
+//  Created by Stanislav Ponomarenko on 2023-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-coordinator-pattern-ios16/swiftui_coordinator_pattern_ios16App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_coordinator_pattern_ios16App.swift
+//  swiftui-coordinator-pattern-ios16
+//
+//  Created by Stanislav Ponomarenko on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_coordinator_pattern_ios16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
